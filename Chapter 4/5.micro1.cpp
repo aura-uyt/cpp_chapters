b9c079425585c7a9ebd8a361c7c85e7582ea26e2
Aura-uyt <aura.uyt@gmail.com>
2024-10-23 13:33:02 +0530

    micro-1

--- Chapter 4/5.micro1.cpp
+++ Chapter 4/5.micro1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int pass1, pass2, user;
+    int pass1, pass2, user, input;
     cout<<"\n\t>>> Welcome to Password Management System <<<"<<endl;
     cout<<"Enter your Username: ";
     cin>>user;
@@ -18,7 +18,18 @@ int main(){
     else{
         cout<<"ERROR: Enter the same Password in both fields.";
     }
-    
+
+    cout<<"\n.........................................................................................................................................\n";
+    cout<<"\nTo get all Your usernames & passwords enter the Master Password\n";
+    cin>> input;
+    int master = 96;
+    if ( input == master )
+    {
+        cout<< "username: " << user << " & password: " << pass1 <<endl;
+    }
+    else{
+        cout<<"WRONG MASTER PASS.  :(((\n";
+    }
 
     cout<<endl;
     return 0;
