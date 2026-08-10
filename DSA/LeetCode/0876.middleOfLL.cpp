add2d31ff48bc7104342bc8913dca5f1ddef517f
gyro <gyro.uyt@gmail.com>
2026-08-10 17:07:43 +0000

    0867 Middle of LL (optimal)

--- DSA/LeetCode/0876.middleOfLL.cpp
+++ DSA/LeetCode/0876.middleOfLL.cpp
@@ -1,9 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class ListNode {
+public:
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-  ListNode *middleNode(ListNode *head) {
+  ListNode *optimal(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+      fast = fast->next->next;
+      slow = slow->next;
+    }
+    return slow;
+  }
+  ListNode *bruteForce(ListNode *head) {
     int count = 0;
     ListNode *temp = head;
     while (temp != nullptr) {
