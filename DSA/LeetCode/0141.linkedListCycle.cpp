66835df8620f29c60507dd0b5eaa8f2d9c497bae
gyro <gyro.uyt@gmail.com>
2026-08-11 20:10:53 +0000

    0141 Cycle in LL (floyd's cycle)

--- DSA/LeetCode/0141.linkedListCycle.cpp
+++ DSA/LeetCode/0141.linkedListCycle.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct ListNode {
@@ -9,8 +9,30 @@ struct ListNode {
 
 class Solution {
 public:
-  bool hasCycle(ListNode *head) {
-
+  // 2 people running on Circular track at different speeds are bound to meet
+  // again, if not then it's not circular, why? think of relative speed if one
+  // is moving v1 and other at v2 and v2 > v2 then relatively person1 is idle
+  // and person2 running at (V2-v1) & bcz it's circle he'll meet for sure
+  bool optimal(ListNode *head) {
+    ListNode *fast = head;
+    ListNode *slow = head;
+    while (slow) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast)
+        return true;
+    }
+    return false;
+  }
+  bool brute(ListNode *head) {
+    unordered_set<ListNode *> store;
+    ListNode *temp = head;
+    while (temp) {
+      if (store.find(temp) != store.end())
+        return true;
+      store.insert(temp);
+      temp = temp->next;
+    }
     return false;
   }
 };
@@ -22,7 +44,7 @@ int main() {
   head->next->next->next = new ListNode(-4);
 
   Solution s1;
-  cout << s1.hasCycle(head) << endl;
+  cout << s1.optimal(head) << endl;
 
   return 0;
 }
